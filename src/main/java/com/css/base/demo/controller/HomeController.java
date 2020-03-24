0018af76d46c2ebc37705909d48a225205969ee4
lixf5409 <ltyfrr540926>
2020-03-24 22:16:40 +0800

    ctrl和html入参

--- src/main/java/com/css/base/demo/controller/HomeController.java
+++ src/main/java/com/css/base/demo/controller/HomeController.java
@@ -3,7 +3,9 @@ package com.css.base.demo.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -18,7 +20,10 @@ public class HomeController {
         return "home/home";
     }
     @RequestMapping("/page")
-    public String page(){
+    public String page(@PathVariable(name = "ctrl") String ctrl, @RequestParam(name = "html") String html,Model model){
+        model.addAttribute("ctrl",ctrl);
+        model.addAttribute("html",html);
+
         return "subHome/subHome";
     }
 }
